167802fbc332ce77c2e713ac834378d4604d0bc0
Luca Cerone <luca.cerone@gmail.com>
2021-12-13 14:59:14 +0100

    Improved readability of day 13 part 2 output

--- day_13-transparent_origami/part_2.py
+++ day_13-transparent_origami/part_2.py
@@ -5,4 +5,5 @@ diagram, instructions  = u.import_instructions("./data/input")
 for instruction in instructions:
     diagram = u.apply_instruction(instruction, diagram)
 
+print("")
 u.print_diagram(diagram)
