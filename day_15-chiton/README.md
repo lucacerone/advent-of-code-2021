d63c6a9e48bd0f94e7b81131551f1c5c4bce86b9
Luca Cerone <luca.cerone@gmail.com>
2021-12-15 23:57:35 +0100

    Day 15 part 1 fast solution

--- day_15-chiton/README.md
+++ day_15-chiton/README.md
@@ -14,3 +14,12 @@ Part 2:
 ```python
 python part_2.py
 ```
+## Useful Resources
+
+My original solution was working, but it took around 1 min to solve part 1.
+
+I reviewed how Dijkstra algorithm works on [Wikipedia](https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm) 
+and on this [Udacity post](https://www.udacity.com/blog/2021/10/implementing-dijkstras-algorithm-in-python.html).
+
+For efficiency using a heap, I followed along [this gist](https://gist.github.com/kachayev/5990802).
+
---
+++ day_15-chiton/part_2.py
@@ -0,0 +1,59 @@
+import utils as u
+import numpy as np
+
+import heapq as hq
+
+
+s = """\
+1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581
+"""
+
+#cave = np.array([[int(x) for x in l] for l in s.splitlines()])
+cave = u.import_data("./data/input")
+
+
+neighbors = lambda pos: u.get_adjacent_coordinates(cave, pos[0], pos[1])
+
+start_point = (0,0)
+end_point = cave.shape[0]-1, cave.shape[1]-1
+
+
+"""
+  Represent the cave matrix as a graph. We define the graph as a dictionary,
+  an entry of which looks like
+    graph[v0] = [(v1, d1), (v2, d2)]
+  indicating that there is an edge between v0 and v1 with d1 distance, and an edge between v0 and v2 with d2 distance
+"""
+graph = {(i,j): [(n, cave[n]) for n in neighbors((i,j))] for i in range(cave.shape[0]) for j in range(cave.shape[1])}
+
+visited = set()
+
+costs = {v: np.Inf for v in graph}
+costs[start_point] = 0
+
+queue = [(costs[start_point], start_point)]
+
+
+while queue:
+    current_cost , current_node = hq.heappop(queue)
+    if current_node in visited:
+        continue
+
+    for neigh,d in graph[current_node]:
+        if neigh not in visited:
+            new_cost = costs[current_node] + d
+            if new_cost < costs[neigh]:
+                costs[neigh] = new_cost
+                hq.heappush(queue, (new_cost, neigh)) 
+    visited.add(current_node)
+
+print(costs[end_point])
